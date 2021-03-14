f904e9ee72fde3a8940a6bc82b7b82f38b3e8502
Kallemakela <kalle.makela@aalto.fi>
2021-03-14 15:24:21 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <h1>
+            Welcome to the Programming basics course!
+          </h1>
+
+          <div className='feature'>
+            <h3>Languages available</h3>
+            <div className='list'>
+              <p> &#62; R</p>
+              <p> &#62; Python</p>
+              <p> &#62; JavaScript</p>
+            </div>
+          </div>
+          
+          <div className='feature'>
+            <h3>Specializations available</h3>
+            <div className='list ml-1'>
+              <p> &#62; Data processing and analysis</p>
+              <p> &#62; Web development</p>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
